797784581222b7963a69e82d3a631f8ad8b4e234
Nikith17 <nikithareddy1799@gmail.com>
2025-02-23 01:15:32 -0500

    Added GitHub Actions workflow for pytest

---
+++ tests/__init__.py
@@ -0,0 +1,9 @@
+# init.py
+
+def area(side_length):
+    """
+    This function calculates the area of a square.
+    :param side_length: The length of one side of the square.
+    :return: Area of the square.
+    """
+    return side_length ** 2
